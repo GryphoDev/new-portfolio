f3c879454590a7ffbbafc9733d83aa4cc90fd81b
GryphoDev <wmauroux@icloud.com>
2025-03-10 11:51:50 +0100

    refactor: darkmode.js

--- js/darkmode.js
+++ js/darkmode.js
@@ -2,24 +2,14 @@ const darkModeButton = document.querySelector(".dark-mode-button");
 
 export function darkMode() {
   // Appliquer le thème au chargement de la page
-  const savedTheme = localStorage.getItem("theme");
-  if (savedTheme === "dark") {
-    document.documentElement.classList.add("dark-mode");
-    darkModeButton.innerHTML = "LIGHT"; // Mettre à jour le texte du bouton
-  } else {
-    darkModeButton.innerHTML = "DARK"; // Par défaut, mode clair
-  }
+  const isDark = localStorage.getItem("theme") === "dark";
+  isDark && document.documentElement.classList.add("dark-mode");
+  darkModeButton.innerHTML = isDark ? "LIGHT" : "DARK";
 
   // Ajouter un écouteur d'événement pour basculer entre les thèmes
   darkModeButton.addEventListener("click", () => {
     const isDarkMode = document.documentElement.classList.toggle("dark-mode"); // Bascule la classe
-
-    if (isDarkMode) {
-      localStorage.setItem("theme", "dark"); // Enregistrer en mode sombre
-      darkModeButton.innerHTML = "LIGHT"; // Mettre à jour le texte du bouton
-    } else {
-      localStorage.setItem("theme", "light"); // Enregistrer en mode clair
-      darkModeButton.innerHTML = "DARK"; // Mettre à jour le texte du bouton
-    }
+    localStorage.setItem("theme", isDarkMode ? "dark" : "light"); // Enregistrer en mode sombre
+    darkModeButton.innerHTML = isDarkMode ? "LIGHT" : "DARK"; // Mettre à jour le texte du bouton
   });
 }
